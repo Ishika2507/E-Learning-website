07a193950e7baa381da75644ff01168316c16559
Ishika2507 <ishikakarna@gmail.com>
2025-03-27 14:06:56 +0530

    basic changes

--- js/script.js
+++ js/script.js
@@ -1,38 +1,12 @@
-let toggleBtn = document.getElementById('toggle-btn');
 let body = document.body;
-let darkMode = localStorage.getItem('dark-mode');
-
-const enableDarkMode = () =>{
-   toggleBtn.classList.replace('fa-sun', 'fa-moon');
-   body.classList.add('dark');
-   localStorage.setItem('dark-mode', 'enabled');
-}
-
-const disableDarkMode = () =>{
-   toggleBtn.classList.replace('fa-moon', 'fa-sun');
-   body.classList.remove('dark');
-   localStorage.setItem('dark-mode', 'disabled');
-}
-
-if(darkMode === 'enabled'){
-   enableDarkMode();
-}
-
-toggleBtn.onclick = (e) =>{
-   darkMode = localStorage.getItem('dark-mode');
-   if(darkMode === 'disabled'){
-      enableDarkMode();
-   }else{
-      disableDarkMode();
-   }
-}
-
 let profile = document.querySelector('.header .flex .profile');
+
 
 document.querySelector('#user-btn').onclick = () =>{
    profile.classList.toggle('active');
    search.classList.remove('active');
 }
+
 
 let search = document.querySelector('.header .flex .search-form');
 
@@ -40,6 +14,7 @@ document.querySelector('#search-btn').onclick = () =>{
    search.classList.toggle('active');
    profile.classList.remove('active');
 }
+
 
 let sideBar = document.querySelector('.side-bar');
 
@@ -49,9 +24,10 @@ document.querySelector('#menu-btn').onclick = () =>{
 }
 
 document.querySelector('#close-btn').onclick = () =>{
-   sideBar.classList.remove('active');
-   body.classList.remove('active');
+   sideBar.classList.toggle('active');
+   body.classList.toggle('active');
 }
+
 
 window.onscroll = () =>{
    profile.classList.remove('active');
@@ -60,5 +36,37 @@ window.onscroll = () =>{
    if(window.innerWidth < 1200){
       sideBar.classList.remove('active');
       body.classList.remove('active');
+   }
+}
+
+let toggleBtn = document.getElementById('toggle-btn');
+let darkMode = localStorage.getItem('dark-mode');
+
+
+const enableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-sun', 'fa-moon');
+   body.classList.add('dark');
+   localStorage.setItem('dark-mode', 'enabled');
+}
+
+
+const disableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-moon', 'fa-sun');
+   body.classList.remove('dark');
+   localStorage.setItem('dark-mode', 'disabled');
+}
+
+
+if(darkMode === 'enabled'){
+   enableDarkMode();
+}
+
+
+toggleBtn.onclick = (e) =>{
+   darkMode = localStorage.getItem('dark-mode');
+   if(darkMode === 'disabled'){
+      enableDarkMode();
+   }else{
+      disableDarkMode();
    }
 }
